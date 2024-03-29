61e32f6bddb00f382f842af6181568d6757ece87
Jaimin Detroja <officialjaimin@gmail.com>
2024-03-29 16:27:32 +0530

    Add home section with beautiful typing animations

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
-// For navigetion bar
-
+// For navigation bar
 let marker = document.querySelector("#marker");
 let item = document.querySelectorAll(".navbar a");
 
-function indicator(e){
-    marker.style.left = e.offsetLeft+"px";
-    marker.style.width = e.offsetWidth+"px";
+function indicator(e) {
+    marker.style.left = e.offsetLeft - 5 + "px";
+    marker.style.width = e.offsetWidth + 10 + "px";
 }
 
 item.forEach(link => {
-    link.addEventListener('click' , (e)=> {
+    link.addEventListener('click', (e) => {
         indicator(e.target);
     })
 })
+
+
+
+
+// typing animation script
+var typed = new Typed(".typing", {
+    strings: ["Computer Engineer" , "Coder", "Developer", "Backend Developer" ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true,
+    fadeOut: false,
+
+});
